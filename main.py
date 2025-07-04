b687c08beed2c870558b1170d7df878af9b4cf43
Bogdanpavelko <bogdanpavelko22@gmail.com>
2025-07-04 14:08:39 +0300

    Поміняв промти

--- main.py
+++ main.py
@@ -74,8 +74,11 @@ def ask_openrouter(prompt):
     data = {  
         "model": "deepseek/deepseek-v3-base:free",
         "messages": [
-            {"role": "user", "content": prompt}
+        {"role": "system", "content": "Відповідай українською мовою коротко і зрозуміло."},
+        {"role": "user", "content": prompt}
         ]
+
+
     }
 
 
